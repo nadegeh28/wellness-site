24d9ad4ab377f10727993b5a487bdaef6739699d
nadegeh28 <nadegensg28@gmail.com>
2024-08-02 13:55:37 +0200

    fin de page ajout

--- wp-content/themes/wellness/page-regimes.php
+++ wp-content/themes/wellness/page-regimes.php
@@ -41,7 +41,7 @@
             <a href="#" class="learnmore">Voir plus ici</a>
         </div>
         <div class="diet-image1">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-14.png" class="imgreg1" alt="Plat Vegan">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-15.png" class="imgreg1" alt="Plat Vegan">
         </div>
     </div>
 
@@ -49,14 +49,46 @@
 
     <div class="diet-section reverse">
         <div class="diet-image2">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-13.png" class="imgreg2" alt="Plat Vegan">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-16.png" class="imgreg2" alt="Plat Vegan">
         </div>
         <div class="diet-content">
-            <h2 class="diet-title diabetic">Diabétique</h2>
-            <p class="textreg">Un régime diabétique maintient la glycémie en surveillant les glucides. Il favorise les aliments à faible indice glycémique tels que les fruits, les légumes et les céréales complètes. Il intègre également des protéines maigres, des graisses saines et des fibres, tout en évitant les sucres ajoutés et les aliments transformés.</p>
+            <h2 class="diet-title diabetic">Perte de poids</h2>
+            <p class="textreg">Un régime pour la perte de poids vise à réduire les calories tout en maintenant une alimentation équilibrée. Il implique souvent la diminution des portions, la réduction des aliments riches en calories et en gras, ainsi que l’augmentation de la consommation de légumes, de protéines maigres et de fibres. L’objectif est de créer un déficit calorique pour encourager la perte de graisse corporelle de manière sûre et saine.</p>
             <br>
             <a href="#" class="learnmore">Voir plus ici</a>
         </div>
+    </div>
+    <div class="diet-section vegan-section">
+        <div class="diet-content">
+            <h2 class="diet-title vegan">Simple</h2>
+            <p class="textreg">Un régime simple se base sur des choix alimentaires naturels et non transformés comme les fruits, les légumes, les céréales complètes et les protéines maigres. Il repose sur la modération des portions, une hydratation adéquate et l’évitement des excès de sucres ajoutés, de graisses saturées et de sodium.</p>
+            <br>
+            <a href="#" class="learnmore">Voir plus ici</a>
+        </div>
+        <div class="diet-image1">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-17.png" class="imgreg1" alt="Plat Vegan">
+        </div>
+    </div>
+
+    <br><br><br><br>
+
+    <div class="diet-section reverse">
+        <div class="diet-image2">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-18.png" class="imgreg2" alt="Plat Vegan">
+        </div>
+        <div class="diet-content">
+            <h2 class="diet-title diabetic">MIND regime</h2>
+            <p class="textreg">Le régime mindful met l’accent sur une alimentation consciente et attentive. Il implique de manger en pleine conscience, en savourant chaque bouchée, en écoutant les signaux de faim et de satiété, en faisant des choix alimentaires éclairés et en cultivant la gratitude envers la nourriture. C’est une approche axée sur une relation plus consciente avec les repas pour favoriser une alimentation saine et équilibrée.</p>
+            <br>
+            <a href="#" class="learnmore">Voir plus ici</a>
+</div>
+        </div>
+        <div class="quizsection">
+        <h2>Besoin d'aide ?</h2>
+        <p>Faites le Quizz et Wellness trouvera le régime parfait pour toi</p>
+        <a href="#">Start</a>
+    
+</div>
     </div>
 </div>
 
