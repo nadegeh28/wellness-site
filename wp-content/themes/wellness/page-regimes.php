2c280ee60734b393ddb1cab8423cf032fb0f9d7a
nadegeh28 <nadegensg28@gmail.com>
2024-08-02 13:27:19 +0200

    ajout image

--- wp-content/themes/wellness/page-regimes.php
+++ wp-content/themes/wellness/page-regimes.php
@@ -12,10 +12,10 @@
         <div class="diet-content">
             <h2 class="diet-title vegan">Vegan</h2>
             <p class="textreg">Le régime vegan exclut toute consommation de produits d'origine animale. Ce régime ne comportent pas de viande, de poisson, de produits laitiers, d'œufs ou de miel. Les repas contiennent principalement des fruits, légumes, céréales, légumineuses, noix et graines pour obtenir tous les nutriments nécessaires. Ce régime est souvent adopté pour des raisons éthiques, de santé ou environnementales.</p>
-            <a href="#" class="learn-more">Voir plus ici</a>
+            <a href="#" class="learnmore">Voir plus ici</a>
         </div>
         <div class="diet-image">
-            <img src="URL_TO_VEGAN_IMAGE" alt="Vegan">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-14.png" class="img-fluid" alt="Plat Vegan" style="width: 150px;">
         </div>
     </div>
 
@@ -27,7 +27,7 @@
         <div class="diet-content">
             <h2 class="diet-title diabetic">Diabétique</h2>
             <p class="textreg">Un régime diabétique maintient la glycémie en surveillant les glucides. Il favorise les aliments à faible indice glycémique tels que les fruits, les légumes et les céréales complètes. Il intègre également des protéines maigres, des graisses saines et des fibres, tout en évitant les sucres ajoutés et les aliments transformés.</p>
-            <a href="#" class="learn-more">Voir plus ici</a>
+            <a href="#" class="learnmore">Voir plus ici</a>
         </div>
     </div>
 </div>
