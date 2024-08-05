144f94cd58539ad7c8383ef8a88abd762822935c
nadegeh28 <nadegensg28@gmail.com>
2024-08-05 11:31:28 +0200

    avancement de cond g

--- wp-content/themes/wellness/page-conditions-generales.php
+++ wp-content/themes/wellness/page-conditions-generales.php
@@ -2,46 +2,64 @@
 
 <div class="conditions-generales-container">
   <h1>CONDITIONS GÉNÉRALES</h1>
+
+  <br><br>
   <p>Bienvenue sur Wellness !</p>
   <p>Veuillez lire attentivement ces Conditions Générales d’Utilisation (ci-après dénommées « CGU ») avant d’utiliser notre site web <a href="http://www.wellness.emu.isfsc.be">www.wellness.emu.isfsc.be</a> (ci-après dénommé le « Site ») exploité par Wellthy (ci-après dénommé “ nous ”, « notre », « nos »).</p>
-  
-  <h2>1. Acceptation des Conditions d'Utilisation</h2>
+  <br>
+  <h4>1. Acceptation des Conditions d'Utilisation</h4>
   <p>En accédant et en utilisant notre Site, vous acceptez pleinement et sans réserve ces CGU. Si vous n’acceptez pas toutes les conditions et modalités de ces CGU, veuillez ne pas utiliser notre Site.</p>
   
-  <h2>2. Objectif du Site</h2>
+  <br>
+  
+  <h4>2. Objectif du Site</h4>
   <p>Le Site Wellness vise à fournir des informations détaillées et actualisées sur la nutrition, la santé et le bien-être. Les contenus disponibles sur ce Site sont destinés à des fins d’information générale et ne remplacent en aucun cas les conseils personnalisés d’un professionnel de la santé qualifié. Nous ne fournissons pas de services de consultation médicale ou de traitement.</p>
   
-  <h2>3. Utilisation du Contenu</h2>
+  <br>
+ 
+  <h4>3. Utilisation du Contenu</h4>
   <p>Nous nous efforçons de maintenir le Site et son contenu à jour, mais nous ne pouvons pas garantir l’exactitude, l’exhaustivité ou l’actualité des informations. L’utilisation des informations fournies relève de votre propre responsabilité.</p>
+ 
+  <br>
   
-  <h2>4. Responsabilité</h2>
+  <h4>4. Responsabilité</h4>
   <p>Nous déclinons toute responsabilité quant aux dommages directs ou indirects résultant de l’utilisation ou de l’impossibilité d’utiliser le Site ou son contenu. Nous ne sommes pas responsables des conséquences découlant de l’application des informations obtenues à partir de ce site. Les décisions basées sur ces informations sont prises à vos propres risques.</p>
   
-  <h2>5. Propriété Intellectuelle</h2>
+  <br>
+ 
+  <h4>5. Propriété Intellectuelle</h4>
   <p>Tous les contenus présents sur ce Site, tels que les textes, les images, les logos, les marques de commerce et autres éléments, sont la propriété exclusive de Wellthy ou de ses concédants de licence, et sont protégés par les lois sur la propriété intellectuelle en vigueur. Aucune partie de ce Site ne peut être reproduite, distribuée ou utilisée à des fins commerciales sans notre consentement écrit préalable.</p>
-  
-  <h2>6. Confidentialité et Données Personnelles</h2>
+ 
+  <br>
+ 
+  <h4>6. Confidentialité et Données Personnelles</h4>
   <p>La collecte et l’utilisation des données personnelles sont régies par notre Politique de Confidentialité, disponible sur le Site. En utilisant notre Site, vous consentez à la collecte, au stockage et à l’utilisation de vos données personnelles conformément à notre Politique de Confidentialité.</p>
   
-  <h2>7. Modifications des CGU</h2>
+  <br>
+  
+  <h4>7. Modifications des CGU</h4>
   <p>Nous nous réservons le droit de modifier ces CGU à tout moment. Les modifications prendront effet dès leur publication sur le Site. Il est de votre responsabilité de consulter régulièrement ces CGU pour rester informé des éventuelles mises à jour.</p>
   
-  <h2>8. Droit Applicable</h2>
+  <br>
+ 
+  <h4>8. Droit Applicable</h4>
   <p>Ces CGU sont régies par les lois en vigueur en Belgique. En cas de litige, les tribunaux compétents seront ceux de la juridiction de Bruxelles.</p>
   
-  <h2>9. Conditions d’Inscription</h2>
+  <br>
+  
+  <h4>9. Conditions d’Inscription</h4>
   <p>L’inscription sur Wellness peut être requise pour accéder à certains contenus ou fonctionnalités. En vous inscrivant sur notre Site, vous acceptez les conditions suivantes :</p>
   
-  <h3>a. Création de Compte</h3>
+  <h5>a. Création de Compte</h5>
   <p>Pour vous inscrire sur Wellness, vous devez fournir des informations exactes, complètes et à jour telles que votre nom, votre adresse e-mail et toute autre information demandée dans le formulaire d’inscription. Vous êtes entièrement responsable de la confidentialité de vos identifiants de connexion et de toutes les activités qui se déroulent sous votre compte.</p>
   
-  <h3>b. Exactitude des Informations</h3>
+  <h5>b. Exactitude des Informations</h5>
   <p>Vous acceptez de maintenir à jour et de corriger promptement toute information fournie lors de votre inscription pour garantir qu’elle soit exacte, complète et à jour. En cas de fourniture d’informations fausses, inexactes, périmées ou incomplètes, nous nous réservons le droit de suspendre ou de résilier votre compte et de refuser l’accès actuel ou futur à notre Site.</p>
   
-  <h3>c. Utilisation du Compte</h3>
+  <h5>c. Utilisation du Compte</h5>
   <p>Votre compte est strictement personnel et vous ne pouvez pas le partager ni le céder à une tierce partie. Vous êtes responsable de toute activité réalisée depuis votre compte. En cas de soupçon d’utilisation non autorisée de votre compte, vous devez nous en informer immédiatement.</p>
   
-  <h3>d. Fermeture de Compte</h3>
+  <h5>d. Fermeture de Compte</h5>
   <p>Vous pouvez à tout moment demander la fermeture de votre compte en nous contactant via les informations fournies sur le Site. La fermeture de votre compte entraîne la suppression de vos données personnelles, sauf dans le cas où la loi ou nos politiques nécessitent la conservation des données l’égard.</p>
   
   <p>En utilisant ce Site, vous acceptez ces CGU dans leur intégralité. Si vous ne les acceptez pas, veuillez ne pas utiliser notre site.</p>
@@ -52,5 +70,7 @@
   
   <p><strong>Date d’entrée en vigueur : 21 décembre 2023</strong></p>
 </div>
+
+<br><br><br><br><br>
 
 <?php get_footer();?>
