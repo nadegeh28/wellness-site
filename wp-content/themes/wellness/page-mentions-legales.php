aedc794f51ac9a99a4f5e16f493a95dd33f8b0be
nadegeh28 <nadegensg28@gmail.com>
2024-08-05 11:52:55 +0200

    css ment leg

--- wp-content/themes/wellness/page-mentions-legales.php
+++ wp-content/themes/wellness/page-mentions-legales.php
@@ -5,11 +5,14 @@
 
     <hr class="title-underline">
 
+    <br><br><br>
+
     <p>Wealthy srl, inscrite à la BCE sous le numéro 4213675908 et dont le siège d’exploitation est situé Rue de la Poste 111, 1030 Bruxelles.</p>
     <p>TVA: BE4213. 675. 908<br>
     E-mail: <a href="mailto:contact@wellness.com">contact@wellness.com</a></p>
 
-    <div class="mentions-legales-container">
 </div>
+
+<br><br><br><br><br><br><br><br><br>
 
 <?php get_footer();?>
