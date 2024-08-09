29d6ecbc044a2812993f6ac5f340711128a168b0
nadegeh28 <nadegensg28@gmail.com>
2024-08-09 15:24:24 +0200

    quizz continue

--- wp-content/themes/wellness/page-quizz.php
+++ wp-content/themes/wellness/page-quizz.php
@@ -3,22 +3,16 @@
 <div class="quizz-container">
     <h2>Combien de repas prenez-vous par jour ?</h2>
     
-    <form method="post" action="#">
+    <form method="post" action="quizz-2.php">
         <ul class="quizz-options">
-            <li><input type="radio" name="repas" id="2repas" value="2repas"><label for="2repas">2 repas</label></li>
-            <li><input type="radio" name="repas" id="3repas" value="3repas"><label for="3repas">3 repas</label></li>
-            <li><input type="radio" name="repas" id="4repas" value="4repas"><label for="4repas">4 repas ou plus</label></li>
-            <li><input type="radio" name="repas" id="3repasCollations" value="3repasCollations"><label for="3repasCollations">3 repas avec des collations</label></li>
-            <li><input type="radio" name="repas" id="pasRegimeFixe" value="pasRegimeFixe"><label for="pasRegimeFixe">Pas de régime fixe</label></li>
+            <li><input type="radio" name="repas1" id="2repas" value="2repas"><label for="2repas">2 repas</label></li>
+            <li><input type="radio" name="repas1" id="3repas" value="3repas"><label for="3repas">3 repas</label></li>
+            <li><input type="radio" name="repas1" id="4repas" value="4repas"><label for="4repas">4 repas ou plus</label></li>
+            <li><input type="radio" name="repas1" id="3repasCollations" value="3repasCollations"><label for="3repasCollations">3 repas avec des collations</label></li>
+            <li><input type="radio" name="repas1" id="pasRegimeFixe" value="pasRegimeFixe"><label for="pasRegimeFixe">Pas de régime fixe</label></li>
         </ul>
         <br><br>
-        <a href="/wellness-site/index.php/quizz-2/" class="quizz-button">Suivant</a>
-
-
-
-
-
-
+        <button type="submit" class="quizz-button">Suivant</button>
     </form>
 
     <div class="quizz-progress">
@@ -30,3 +24,4 @@
 </div>
 
 <?php get_footer(); ?>
+
