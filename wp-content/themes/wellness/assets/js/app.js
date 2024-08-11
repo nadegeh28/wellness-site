ef1b218e107981a3f7fb2b97ed5cb1479fe1f7d1
nadegeh28 <nadegensg28@gmail.com>
2024-08-11 09:34:32 +0200

    finalisation

--- wp-content/themes/wellness/assets/js/app.js
+++ wp-content/themes/wellness/assets/js/app.js
@@ -1,15 +1,14 @@
-
 function saveRecipe(button) {
-    if (!button.classList.contains('saved')) {
-        // Enregistrer l'état
-        button.classList.add('saved');
 
-        // Délai avant de se désenregistrer
-        setTimeout(() => {
-            button.classList.remove('saved');
-        }, 3000); // Délai de 3 secondes
-    } else {
-        // Annuler l'enregistrement si déjà enregistré
+    const isSaved = button.classList.contains('saved');
+
+
+    if (isSaved) {
+
         button.classList.remove('saved');
+    } else {
+
+        button.classList.add('saved');
     }
 }
+
