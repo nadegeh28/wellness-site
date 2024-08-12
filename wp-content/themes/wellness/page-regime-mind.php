61e85d48855a96a7abbd02e726d93adbfc084bfc
nadegeh28 <nadegensg28@gmail.com>
2024-08-12 17:59:25 +0200

    cré et finalisation reg mind

---
+++ wp-content/themes/wellness/page-regime-mind.php
@@ -0,0 +1,174 @@
+<?php get_header();?>
+
+<section class="regime-section">
+    <div class="section-title">
+        <h2>MIND Régime</h2>
+    </div>
+    <div class="content">
+        <div class="text">
+<p>Le régime MIND, qui signifie « Mediterranean-DASH Intervention for Neurodegenerative Delay » (intervention méditerranéenne-DASH pour le retard neurodégénératif), combine les principes du régime méditerranéen et du régime DASH (Dietary Approaches to Stop Hypertension). Son objectif principal est de soutenir la santé du cerveau et de réduire le risque de déclin cognitif lié à l’âge.</p>
+<p>Ce régime repose sur des recherches indiquant que certains aliments peuvent avoir des effets positifs sur la santé du cerveau, notamment en réduisant l’inflammation et en protégeant les cellules cérébrales. Il encourage une alimentation riche en nutriments essentiels, en antioxydants et en graisses saines pour soutenir la fonction cérébrale et la santé générale.</p>
+<p>L’approche du régime MIND est souple et adaptable, ce qui permet aux individus de suivre les principes de base tout en personnalisant leurs choix alimentaires en fonction de leurs préférences et de leurs besoins spécifiques.</p>
+        </div>
+        <div class="image">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-18.png" class="imgregime">
+        </div>
+    </div>
+
+    <section class="recipes">
+        <h2>Recettes</h2>
+        <br><br>
+        <div class="recipe-cards">
+            <div class="recipe-card">
+            <button class="save-button" onclick="saveRecipe(this)"></button>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-11.png" alt="Muesli aux fruits" class="repas">
+<h3>Céréales avec des amandes et des fruits</h3>
+<img src="<?php echo get_template_directory_uri(); ?>/assets/img/stars.png" alt="stars" class="etoiles">
+                <p class="ingredients">3 œufs</p>
+                    <p class="ingredients">50g de céréales complètes</p>
+                    <p class="ingredients">20g d'amandes</p>
+                    <p class="ingredients">1/2 banane</p>
+                    <p class="ingredients">100g de fraises</p>
+                    <p class="ingredients">100g de myrtilles</p>
+                    <p class="ingredients">150ml de lait ou yaourt</p>
+                    <p class="ingredients">Une pincée de canelle ou miel</p>
+                <p class="prep">Préparation : 10 min</p>
+            </div>
+            <div class="recipe-card">
+            <button class="save-button" onclick="saveRecipe(this)"></button>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-10.png" alt="Curry de légumes">
+                <h3>Pudding de chia aux fruits rouges</h3>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/stars.png" alt="stars" class="etoiles">
+                    <p class="ingredients">30g de graines de chia</p>
+                    <p class="ingredients">250ml de lait d'amande</p>
+                    <p class="ingredients">1 cuillère à café de miel</p>
+                    <p class="ingredients">150g de fruits rouges</p>
+                    <p class="ingredients">Quelques gouttes d'extrait de vanille</p>
+                <p class="prep">Préparation : 10 min</p>
+            </div>
+            <div class="recipe-card">
+            <button class="save-button" onclick="saveRecipe(this)"></button>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-12.png" alt="Wrap végétalien">
+                <h3>Pain avec de la banane et aux noix</h3>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/stars.png" alt="stars" class="etoiles">
+                    <p class="ingredients">2 tranches de pain de seigle</p>
+                    <p class="ingredients">1 banane</p>
+                    <p class="ingredients">30g de noix</p>
+                    <p class="ingredients">2 cuillères à soupe de beure d'amande</p>
+                    <p class="ingredients">Une pincée de cacahuète</p>
+                <p class="prep">Préparation : 5 min</p>
+            </div>
+        </div>
+     </div>  
+</section> 
+
+    <section class="recipes">
+     <div class="recipe-cards">
+            <div class="recipe-card">
+            <button class="save-button" onclick="saveRecipe(this)"></button>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-10.png" alt="Curry de légumes">
+                <h3>Pâtes à la sauce à l'ail et aux légumes verts</h3>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/stars.png" alt="stars" class="etoiles">
+                    <p class="ingredients"> de spaghetti</p>
+                    <p class="ingredients">3 gousses d'ail</p>
+                    <p class="ingredients">200g de brocoli</p>
+                    <p class="ingredients">100g d'épinards frais</p>
+                    <p class="ingredients">1 courgette</p>
+                    <p class="ingredients">2 cuillère à soupe d'huile d'olive</p>
+                    <p class="ingredients">Une pincée de pimennt</p>
+                    <p class="ingredients">30g de parmesan râpé/p>
+                    <p class="ingredients">1 tomate</p>
+                <p class="prep">Préparation : 25 min</p>
+            </div>
+            <div class="recipe-card">
+            <button class="save-button" onclick="saveRecipe(this)"></button>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-12.png" alt="Wrap végétalien">
+                <h3>Tortilla au poisson avec salade de chou</h3>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/stars.png" alt="stars" class="etoiles">
+                    <p class="ingredients">300g de filets de poisson</p>
+                    <p class="ingredients">4 tortillas de maïs</p>
+                    <p class="ingredients">1 cuillère à soupe d'huile d'olive</p>
+                    <p class="ingredients">1 cuillère à café</p>
+                    <p class="ingredients">1 cuillère à café de cumin</p>
+                    <p class="ingredients">Quelques gouttes de jus de citron</p>
+                    <p class="ingredients">100g de farine</p>
+                    <p class="ingredients">200g de chou blanc</p>
+                    <p class="ingredients">1 carotte</p>
+                    <p class="ingredients">1 poignée de coriandre</p>
+                <p class="prep">Préparation : 25 min</p>
+            </div>
+            <div class="recipe-card">
+            <button class="save-button" onclick="saveRecipe(this)"></button>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-10.png" alt="Curry de légumes">
+                <h3>Salade de saumon et avocat</h3>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/stars.png" alt="stars" class="etoiles">
+                    <p class="ingredients">200g de saumon cuit</p>
+                    <p class="ingredients">1 avocat</p>
+                    <p class="ingredients">100g de laitue</p>
+                    <p class="ingredients">150g de tomates cerises</p>
+                    <p class="ingredients">1/2 concombre</p>
+                    <p class="ingredients">1/4 oignon rouge</p>
+                    <p class="ingredients">2 cuillères à café du jus de citron</p>
+                    <p class="ingredients">1 cuillère d'huile d'olive</p>
+                    <p class="ingredients">1 poignée de coriandre</p>
+                <p class="prep">Préparation : 15 min</p>
+            </div>
+        </div>
+     </div>  
+</section>
+
+     <section class="recipes">
+     <div class="recipe-cards">
+            <div class="recipe-card">
+            <button class="save-button" onclick="saveRecipe(this)"></button>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-10.png" alt="Curry de légumes">
+                <h3>Burgers au poulet et à l'avocat</h3>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/stars.png" alt="stars" class="etoiles">
+                    <p class="ingredients">2 pains de burger complet</p>
+                    <p class="ingredients">1 avocat</p>
+                    <p class="ingredients">Quelques feuilles de laitue</p>
+                    <p class="ingredients">1 tomate</p>
+                    <p class="ingredients">1 cuillère à café</p>
+                    <p class="ingredients">1/2 oignon rouge</p>
+                    <p class="ingredients">Sauce au goût</p>
+                    <p class="ingredients">400g de filets de poulet</p>
+             <p class="prep">Préparation : 35 min</p>
+            </div>
+            <div class="recipe-card">
+            <button class="save-button" onclick="saveRecipe(this)"></button>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-12.png" alt="Wrap végétalien">
+                <h3>Pâtes aux courgettes avec sauce maison</h3>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/stars.png" alt="stars" class="etoiles">
+                    <p class="ingredients">200g de pâtes complètes</p>
+                    <p class="ingredients">2 courgettes</p>
+                    <p class="ingredients">400g de tomates</p>
+                    <p class="ingredients">1 cuillère à soupe d'huile d'olive</p>
+                    <p class="ingredients">1 cuillère à soupe de basilic frais</p>
+                    <p class="ingredients">Une pincée de piment</p>
+                    <p class="ingredients">1 oignon vert</p>
+                <p class="prep">Préparation : 30 min</p>
+            </div>
+            <div class="recipe-card">
+            <button class="save-button" onclick="saveRecipe(this)"></button>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/image-12.png" alt="Wrap végétalien">
+                <h3>Bœuf sauté aux légumes</h3>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/stars.png" alt="stars" class="etoiles">
+                    <p class="ingredients">400g de tofu ferme</p>
+                    <p class="ingredients">1 poivron vert</p>
+                    <p class="ingredients">1 courgette</p>
+                    <p class="ingredients">1 oignon rouge</p>
+                    <p class="ingredients">200g de champignons</p>
+                    <p class="ingredients">2 cuillères à soupe d'huile d'olive</p>
+                    <p class="ingredients">3 cuillères à soupe de sauce soja</p>
+                    <p class="ingredients">2 gousses d'ail</p>
+                    <p class="ingredients">1 cuillères à soupe de jus de citron</p>
+                <p class="prep">Préparation : 30 min</p>
+            </div>
+        </div>
+     </div>   
+     
+    </section>
+
+
+
+<?php get_footer();?>
