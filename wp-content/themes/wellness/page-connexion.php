15e4f511cf0baca8c73a2587c51c896320abdc5a
nadegeh28 <nadegensg28@gmail.com>
2024-08-17 15:22:23 +0200

    finalisation pg log in crea profil

--- wp-content/themes/wellness/page-connexion.php
+++ wp-content/themes/wellness/page-connexion.php
@@ -3,18 +3,19 @@
 
 
 
-<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logowelleness.png" alt="Wellness Logo" class="logo-inscr">
+
 <form action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="post" class="login-form">   
+<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logowelleness.png" alt="Wellness Logo" class="logo-con">
 <input type="hidden" name="action" value="custom_login">
     
-    <input type="email" name="user_email" id="email" placeholder="votre adresse e-mail" required class="email">
+    <input type="email" name="user_email" id="email" placeholder="Adresse e-mail" required class="email">
     
-    <input type="password" name="user_password" id="password" placeholder="votre mot de passe" required class="password">
+    <input type="password" name="user_password" id="password" placeholder="Mot de passe" required class="password">
     
     <?php wp_nonce_field( 'custom_login_nonce', 'custom_login_nonce_field' ); ?>
-    
-    <input type="submit" value="Se connecter" class="login-button">
+
 </form>
+<input type="submit" value="Se connecter" class="login-button">
 
 
 <?php get_footer();?>
