def07fefbd1e2f3a10b223e5578e5eea0f1066d4
nadegeh28 <nadegensg28@gmail.com>
2024-08-20 17:01:30 +0200

    ajout de photo article & suppression d'ancienne

--- wp-content/themes/wellness/page-article-1.php
+++ wp-content/themes/wellness/page-article-1.php
@@ -1,3 +1,84 @@
 <?php get_header();?>
 
+    <div class="container">
+        <article>
+            <h3 class="titreblog">Les Avantages et Défis du Régime Vegan : Une Analyse Approfondie</h3>
+            <div class="image-container">
+                <img src="images/fruits.jpg" alt="Fruits et Papaye">
+                <p>
+                Les régimes végétariens, qui excluent la consommation de viande et parfois d’autres produits d’origine animale, sont devenus de plus en plus populaires dans le monde entier. Que ce soit pour des raisons de santé, environnementales ou éthiques, de nombreuses personnes choisissent ce mode de vie alimentaire. Dans cet article, nous explorerons les différents types de régimes végétariens, leurs avantages, ainsi que des conseils pratiques pour suivre ce régime de manière équilibrée.
+                </p>
+            </div>
+            
+            <section>
+                <p class="tilteblog">Différents Types de Régimes Végétariens</h2>
+                <p class="tilteblog">Lacto-Végétarisme :</p>
+                <p>
+                    Le régime lacto-végétarien est l'un des plus répandus. Il exclut la viande, le poisson, et les œufs, mais autorise les produits laitiers comme le lait et le fromage. Ce régime est apprécié pour sa capacité à fournir des nutriments essentiels tout en limitant la consommation de produits animaux.
+                </p>
+                
+                <p class="tilteblog">Ovo-Végétarisme :</p>
+                <p>
+                    Le régime ovo-végétarien évite la viande, le poisson et les produits laitiers, mais inclut les œufs. Cela constitue une source importante de protéines pour ceux qui ne consomment pas de produits laitiers.
+                </p>
+                
+                <p class="tilteblog">Lacto-Ovo-Végétarisme :</p>
+                <p>
+                    Le régime lacto-ovo-végétarien combine la consommation de produits laitiers et d'œufs tout en excluant la viande et le poisson. Ce type de régime est souvent recommandé car il équilibre l'apport en protéines tout en minimisant les risques associés à une consommation excessive de viande.
+                </p>
+                
+                <p class="tilteblog">Végétalisme :</p>
+                <p>
+                    Le régime végétalien exclut tous les produits d'origine animale, y compris les produits laitiers, les œufs et le miel. Ce régime repose principalement sur des fruits, légumes, céréales complètes, noix et graines, souvent adopté pour des raisons éthiques, écologiques ou de santé.
+                </p>
+
+                <p class="tilteblog">Avantages des Régimes Végétariens :</p>
+               
+                <p class="tilteblog">Amélioration de la Santé :</p>
+                <p>
+                Les régimes végétariens sont associés à divers avantages pour la santé. Ils sont souvent riches en fibres, en vitamines et en antioxydants, tout en étant faibles en graisses saturées. De nombreuses études montrent que les végétariens ont un risque réduit de maladies cardiovasculaires, d’hypertension artérielle, de diabète de type 2, et de certains types de cancer. Une alimentation riche en fruits, légumes et légumineuses peut contribuer à une meilleure santé globale et à une longévité accrue.
+                </p>
+
+                <p class="tilteblog">Gestion du poids :</p>
+                <p>
+                Les personnes suivant un régime végétarien ont souvent un indice de masse corporelle (IMC) plus bas que celles qui consomment de la viande. Les aliments végétariens sont généralement moins caloriques et plus riches en nutriments, ce qui peut aider à la gestion du poids. En intégrant des aliments à faible densité calorique comme les légumes et les fruits, les végétariens peuvent se sentir rassasiés tout en consommant moins de calories.
+                </p>
+
+
+                <p class="tilteblog">Impact Environnemental Réduit :
+                :</p>
+                <p>
+                Adopter un régime végétarien peut également contribuer à réduire l’impact environnemental de votre alimentation. La production de viande est l’une des principales sources d’émissions de gaz à effet de serre, de déforestation et de consommation d’eau. En réduisant la consommation de produits d’origine animale, vous contribuez à la préservation de l’environnement et à la réduction de votre empreinte écologique.
+
+                </p>
+
+                <p class="tilteblog">Conseils pour Suivre un Régime Végétarien :</p>
+
+                <p class="tilteblog">Planification des Repas :</p>
+                <p>
+                Pour s’assurer que votre alimentation est équilibrée, il est important de planifier vos repas. Incluez une variété de fruits, légumes, légumineuses, grains entiers, noix et graines dans votre alimentation quotidienne. La planification permet de garantir que vous obtenez tous les nutriments nécessaires et de diversifier vos sources de protéines.
+
+                </p>
+
+
+                <p class="tilteblog">Surveillance des Nutriments :</p>
+                <p>
+                Certains nutriments peuvent être plus difficiles à obtenir dans un régime végétarien, notamment la vitamine B12, le fer, le calcium, et les oméga-3. Il peut être nécessaire de prendre des suppléments ou de consommer des aliments enrichis pour éviter les carences. Consultez un nutritionniste pour vous assurer que vos besoins nutritionnels sont satisfaits.
+                </p>
+
+
+                <p class="tilteblog">Variez les Aliments :</p>
+                <p>
+                Certains nutriments peuvent être plus difficiles à obtenir dans un régime végétarien, notamment la vitamine B12, le fer, le calcium, et les oméga-3. Il peut être nécessaire de prendre des suppléments ou de consommer des aliments enrichis pour éviter les carences. Consultez un nutritionniste pour vous assurer que vos besoins nutritionnels sont satisfaits.
+                </p>
+
+
+
+            </section>
+            
+
+
+
+
+
 <?php get_footer();?>
